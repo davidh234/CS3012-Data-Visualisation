f247806251ff799e3cbb4bb7ad2edfb53c90a5ed
David <davidhooban@hotmail.com>
2019-11-18 19:40:29 +0000

    figured out how to add different pages based on the filepath, so added a test page found at localhost:3000/test

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const port = 3000
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
+app.get('/test', (req, res) => res.render('test'))
 
 app.listen(port, () => console.log(`visualisation application listening on port ${port}! Go to http://localhost:3000`))
