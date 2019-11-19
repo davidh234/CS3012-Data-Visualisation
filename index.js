f27241c59dd435d907f18e8d1582ac42ff8b7515
David <davidhooban@hotmail.com>
2019-11-19 19:06:22 +0000

    got github API JSON response to display on the /test page

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+var github = require('octonode');
 const app = express()
 const port = 3000
+var client = github.client();
+
+var ghrepo = client.repo('davidh234/Software-Engineering');
+
+var repoInfo = ghrepo.commits(callback);
+
+function callback() {
+	var ResponseBody;
+	client.get('/users/davidh234', {}, function (err, status, body, headers) { ResponseBody = body; });
+ 	app.get('/test', (req, res) => res.json(ResponseBody));
+}
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('index'))
-app.get('/test', (req, res) => res.render('test'))
 
-app.listen(port, () => console.log(`visualisation application listening on port ${port}! Go to http://localhost:3000`))
+app.listen(port, () => console.log(`Visualisation application listening on port ${port}! Go to http://localhost:3000`))
--- testAPI.js
+++ testAPI.js
@@ -3,7 +3,7 @@ var github = require('octonode');
 var client = github.client();
 
 client.get('/users/davidh234', {}, function (err, status, body, headers) {
-  //console.log(body); //json object
+  console.log(body); //json object
 });
 
 var ghuser         = client.user('davidh234');
@@ -11,6 +11,6 @@ var ghrepo         = client.repo('davidh234/Software-Engineering');
 
 var x = ghrepo.commits(callback);
 
-function callback() { console.log(x); }
+function callback() {}
 
 var ghsearch = client.search();
