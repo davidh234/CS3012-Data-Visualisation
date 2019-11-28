35e1299503de1a32b14ee3e4138b4d11fea54299
David <davidhooban@hotmail.com>
2019-11-28 22:19:30 +0000

    added the barchart grid and reading JSON into it now instead of hardcoded

--- index.js
+++ index.js
@@ -4,34 +4,24 @@ const app = express()
 const port = 3000
 var client = github.client('b24c740b99170487d41248539a4f69b014fb6266');
 
-var linuxRepo = client.repo('torvalds/linux');
-//var reactRepo = client.repo('facebook/react');
-//var tensorflowRepo = client.repo('tensorflow/tensorflow');
-//var bootstrapRepo = client.repo('twbs/bootstrap');
-//var vueRepo = client.repo('vuejs/vue');
-//var d3Repo = client.repo('d3/d3');
-//var vscodeRepo = client.repo('microsoft/vscode');
-//var electronRepo = client.repo('electron/electron');
-//var goRepo = client.repo('golang/go');
-//var nodeRepo = client.repo('nodejs/node');
-
-app.get('/', (req, res) => res.render('index'))
-app.get('/linux', (req, res) => { linuxRepo.contributorsStats((errors, body, headers) => res.send(body)) });
-
-linuxRepo.contributorsStats((errors, body, headers) => extractContributorRepos(body));
-
-
-//given the body of a repository stats page, will extract all top contributors repos list from this JSON
-function extractContributorRepos(body) {
-
-	for (var i = 0; i < body.length; i++) {
-		console.log(body[i].author.repos_url);
-		//var jsonData = JSON.parse(body[i]);
-    	//var author = jsonData.author;
-    	//console.log(author);
-	}
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
 }
+app.use(allowCrossDomain);
 
-app.set('view engine', 'ejs')
+app.use(express.static('data'));
+
+app.set('view engine', 'ejs');
+
+const fs = require('fs');
+
+var JSONdata;
+
+fs.readFile('Output.json', "utf8", (err, data) => { JSONdata = data; console.log(JSONdata)});
+
+app.get('/', (req, res) => res.sendFile("C:/Users/david/Documents/College Work/Third Year/Software Engineering/visualisation/Output.json"));
+
 
 app.listen(port, () => console.log(`Visualisation application listening on port ${port}! Go to http://localhost:3000`))
